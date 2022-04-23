96914d981fd65bb2525ccd1eef63c3d80b6891a6
unknown <sizwenkala80@gmail.com>
2022-04-23 14:38:59 +0200

    make it better

--- server.js
+++ server.js
@@ -2,41 +2,40 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const assert = require('assert');
-const fs = require('fs');
+//const assert = require('assert');
 
-app.set('view engine', 'ejs');
 
-app.use(require('body-parser')());
+
+app.use(express.json())
 
 app.use(express.static("mzizi"))
 
 // FORM SUBMITIONS
 
-const url='mongodb://localhost:27017/mziziProject';
+//const url='mongodb://localhost:27017/mziziProject';
 
-app.get('/insert', function(req, res){
+//app.get('/insert', function(req, res){
 
- res.render('insert', { csrf: 'CSRF token goes here' });
-});
-app.post('/insert', function(req, res){
-let item={
-  name: req.body.name,
-  surname: req.body.surname,
-  email: req.body.email
-};
+// res.render('insert', { csrf: 'CSRF token goes here' });
+//});
+//app.post('/insert', function(req, res){
+//let item={
+//  name: req.body.name,
+//  surname: req.body.surname,
+//  email: req.body.email
+//};
 
 
-mongoose.connect(url, (err,db)=>{
-  assert.equal(null, err);
-  db.collection('clientData').insertOne(item,(err,result)=>{
-    assert.equal(null,err);
-    console.log('item inserted');
-    db.close();
-  });
-});
- res.redirect(303, '/mzizi.html');
-});
+//mongoose.connect(url, (err,db)=>{
+//  assert.equal(null, err);
+//  db.collection('clientData').insertOne(item,(err,result)=>{
+//    assert.equal(null,err);
+  //  console.log('item inserted');
+  //  db.close();
+  //});
+//});
+// res.redirect(303, '/mzizi.html');
+//});
 
 
 
