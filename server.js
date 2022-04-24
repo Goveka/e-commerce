5647bc0d90f8f258116e020c52e101d0b023e64f
unknown <sizwenkala80@gmail.com>
2022-04-24 15:15:57 +0200

    make it better

--- server.js
+++ server.js
@@ -2,46 +2,18 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port=process.ENV || 3001
-//const assert = require('assert');
+const port=process.env.PORT || 3001
 
+
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/mzizi.html')
+})
 
 
 app.use(express.json())
 
 app.use(express.static("mzizi"))
-
-// FORM SUBMITIONS
-
-//const url='mongodb://localhost:27017/mziziProject';
-
-//app.get('/insert', function(req, res){
-
-// res.render('insert', { csrf: 'CSRF token goes here' });
-//});
-//app.post('/insert', function(req, res){
-//let item={
-//  name: req.body.name,
-//  surname: req.body.surname,
-//  email: req.body.email
-//};
-
-
-//mongoose.connect(url, (err,db)=>{
-//  assert.equal(null, err);
-//  db.collection('clientData').insertOne(item,(err,result)=>{
-//    assert.equal(null,err);
-  //  console.log('item inserted');
-  //  db.close();
-  //});
-//});
-// res.redirect(303, '/mzizi.html');
-//});
-
-
-
-
-
 
 
 
